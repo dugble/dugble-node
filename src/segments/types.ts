b5139ecf81e7abdb4b76d0f61749c3710c6fc16b
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-10 18:33:18 +0000

    feat: add segment types

---
+++ src/segments/types.ts
@@ -0,0 +1,31 @@
+export interface Segment {
+  id: string;
+  team_id: string;
+  name: string;
+  created_at: string;
+}
+
+export interface SegmentContact {
+  id: string;
+  team_id: string;
+  email: string;
+  first_name?: string;
+  last_name?: string;
+  unsubscribed: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface CreateSegmentOptions {
+  name: string;
+}
+
+export interface ListSegmentsOptions {
+  limit?: number;
+  offset?: number;
+}
+
+export interface ListSegmentContactsOptions {
+  limit?: number;
+  offset?: number;
+}
